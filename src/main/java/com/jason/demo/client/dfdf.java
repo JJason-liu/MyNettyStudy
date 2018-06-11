63d7ef7fe0c5b6ec7ef1b06852b5b8d36bdd28ae
JJason-liu <765798166@qq.com>
2018-06-11 10:25:34 +0800

    删除测试代码

--- src/main/java/com/jason/demo/client/dfdf.java
+++
@@ -1,9 +0,0 @@
-package com.jason.demo.client;
-
-/**
- * Author:有泪的北极星
- * Date:2018/6/11
- * Time:9:52
- */
-public class dfdf {
-}
--- src/main/java/com/jason/demo/client/yv.java
+++
@@ -1,9 +0,0 @@
-package com.jason.demo.client;
-
-/**
- * Author:有泪的北极星
- * Date:2018/6/11
- * Time:9:38
- */
-public class yv {
-}
